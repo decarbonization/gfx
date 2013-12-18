123b8a4a34b449f143f87f1c08734f840288b0a0
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2013-12-17 21:21:27 -0800

    Uninlined FilePaths methods

--- Source/filepaths.cpp
+++ Source/filepaths.cpp
@@ -12,4 +12,75 @@ namespace gfx {
     //Everything is Unix-derived right now.
     const String *const FilePaths::kPathSeparatorToken = new String("/");
     const String *const FilePaths::kPathExtensionToken = new String(".");
+    
+    const String *FilePaths::normalizePath(const String *path)
+    {
+        gfx_assert_param(path);
+        
+        String *newPath = autoreleased(copy(path));
+        if(!newPath->hasPrefix(kPathSeparatorToken))
+            newPath->insert(kPathSeparatorToken, 0);
+        
+        if(newPath->hasSuffix(kPathSeparatorToken))
+            newPath->deleteRange(Range(newPath->length() - kPathSeparatorToken->length(), kPathSeparatorToken->length()));
+        
+        return newPath;
+    }
+    
+    const String *FilePaths::pathExtension(const String *path)
+    {
+        gfx_assert_param(path);
+        
+        Range locationOfSeparator = path->find(kPathExtensionToken, Range(0, path->length()), kCFCompareBackwards);
+        if(locationOfSeparator.location != kCFNotFound)
+            return path->substring(Range(locationOfSeparator.max(), path->length() - locationOfSeparator.location - locationOfSeparator.length));
+        else
+            return String::Empty;
+    }
+    
+    const String *FilePaths::lastPathComponent(const String *path)
+    {
+        gfx_assert_param(path);
+        
+        Range locationOfSeparator = path->find(kPathSeparatorToken, Range(0, path->length()), kCFCompareBackwards);
+        if(locationOfSeparator.location != kCFNotFound)
+            return path->substring(Range(locationOfSeparator.max(), path->length() - locationOfSeparator.location - locationOfSeparator.length));
+        else
+            return path;
+    }
+    
+    const String *FilePaths::combinePaths(const String *path1, const String *path2)
+    {
+        gfx_assert_param(path1);
+        gfx_assert_param(path2);
+        
+        String::Builder result;
+        
+        result << FilePaths::normalizePath(path1);
+        result << FilePaths::normalizePath(path2);
+        
+        return result;
+    }
+    
+    const String *FilePaths::deleteLastPathComponent(const String *path)
+    {
+        gfx_assert_param(path);
+        
+        Range locationOfSeparator = path->find(kPathSeparatorToken, Range(0, path->length()), kCFCompareBackwards);
+        if(locationOfSeparator.location != kCFNotFound)
+            return path->substring(Range(0, locationOfSeparator.location));
+        else
+            return path;
+    }
+    
+    const String *FilePaths::deletePathExtension(const String *path)
+    {
+        gfx_assert_param(path);
+        
+        Range locationOfSeparator = path->find(kPathExtensionToken, Range(0, path->length()), kCFCompareBackwards);
+        if(locationOfSeparator.location != kCFNotFound)
+            return path->substring(Range(0, locationOfSeparator.location));
+        else
+            return path;
+    }
 }
--- Source/filepaths.h
+++ Source/filepaths.h
@@ -30,19 +30,7 @@ namespace gfx {
         ///
         /// \result A new copy of path conforming to the format `str("/path/components")`.
         ///
-        static const String *normalizePath(const String *path)
-        {
-            gfx_assert_param(path);
-            
-            String *newPath = autoreleased(copy(path));
-            if(!newPath->hasPrefix(kPathSeparatorToken))
-                newPath->insert(kPathSeparatorToken, 0);
-            
-            if(newPath->hasSuffix(kPathSeparatorToken))
-                newPath->deleteRange(Range(newPath->length() - kPathSeparatorToken->length(), kPathSeparatorToken->length()));
-            
-            return newPath;
-        }
+        static const String *normalizePath(const String *path);
         
         ///Returns the extension of a given path string.
         ///
@@ -50,32 +38,14 @@ namespace gfx {
         ///
         /// \result The extension of the path, or an empty string if there was no extension.
         ///
-        static const String *pathExtension(const String *path)
-        {
-            gfx_assert_param(path);
-            
-            Range locationOfSeparator = path->find(kPathExtensionToken, Range(0, path->length()), kCFCompareBackwards);
-            if(locationOfSeparator.location != kCFNotFound)
-                return path->substring(Range(locationOfSeparator.max(), path->length() - locationOfSeparator.location - locationOfSeparator.length));
-            else
-                return String::Empty;
-        }
+        static const String *pathExtension(const String *path);
         
         ///Returns the last component of a given path string.
         ///
         /// \param  path    The path whose last component is to be returned. May not be null.
         ///
         /// \result The last path component. Returns `path` if there are no path separators in the string.
-        static const String *lastPathComponent(const String *path)
-        {
-            gfx_assert_param(path);
-            
-            Range locationOfSeparator = path->find(kPathSeparatorToken, Range(0, path->length()), kCFCompareBackwards);
-            if(locationOfSeparator.location != kCFNotFound)
-                return path->substring(Range(locationOfSeparator.max(), path->length() - locationOfSeparator.location - locationOfSeparator.length));
-            else
-                return path;
-        }
+        static const String *lastPathComponent(const String *path);
         
         ///Returns a new path constructed from two other given paths.
         ///
@@ -84,18 +54,7 @@ namespace gfx {
         ///
         /// \result A string consisting of the combined and normalized contents of both given paths.
         ///
-        static const String *combinePaths(const String *path1, const String *path2)
-        {
-            gfx_assert_param(path1);
-            gfx_assert_param(path2);
-            
-            String::Builder result;
-            
-            result << FilePaths::normalizePath(path1);
-            result << FilePaths::normalizePath(path2);
-            
-            return result;
-        }
+        static const String *combinePaths(const String *path1, const String *path2);
         
         ///Returns a new path by deleting the last path component of a given path.
         ///
@@ -104,16 +63,7 @@ namespace gfx {
         /// \result The path string passed, minus the last path component.
         ///         If there are no path separator tokens, this method returns the path.
         ///
-        static const String *deleteLastPathComponent(const String *path)
-        {
-            gfx_assert_param(path);
-            
-            Range locationOfSeparator = path->find(kPathSeparatorToken, Range(0, path->length()), kCFCompareBackwards);
-            if(locationOfSeparator.location != kCFNotFound)
-                return path->substring(Range(0, locationOfSeparator.location));
-            else
-                return path;
-        }
+        static const String *deleteLastPathComponent(const String *path);
         
         ///Returns a new path by deleting the path extension of a given path.
         ///
@@ -122,16 +72,7 @@ namespace gfx {
         /// \result The path string passed, minus the path extension.
         ///         If there is no path extension, this method returns the path.
         ///
-        static const String *deletePathExtension(const String *path)
-        {
-            gfx_assert_param(path);
-            
-            Range locationOfSeparator = path->find(kPathExtensionToken, Range(0, path->length()), kCFCompareBackwards);
-            if(locationOfSeparator.location != kCFNotFound)
-                return path->substring(Range(0, locationOfSeparator.location));
-            else
-                return path;
-        }
+        static const String *deletePathExtension(const String *path);
         
     private:
         
